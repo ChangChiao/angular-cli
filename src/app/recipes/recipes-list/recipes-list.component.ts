6fdbfab8b4dbcd5c9ad28f876830ddc8d2100c32
joe <joe.chang1014@gmail.com>
2023-02-10 20:36:16 +0800

    feat model

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
-
+import { Recipe } from "../recipe.model";
 @Component({
   selector: 'app-recipes-list',
   templateUrl: './recipes-list.component.html',
   styleUrls: ['./recipes-list.component.scss']
 })
 export class RecipesListComponent {
+  recipes: Recipe[] = [
+    new Recipe('noddle', 'test!!!!', 'https://fakeimg.pl/250x100/'),
+    new Recipe('rice', 'test2!!!!', 'https://fakeimg.pl/250x100/')
+  ]
 
+  constructor() {
+
+  }
 }
